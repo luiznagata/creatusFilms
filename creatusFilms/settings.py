1e4f06e5218cdae30ae871be79d4c8782dd818c2
Luiz Nagata <luiznagata@hotmail.com>
2022-09-04 16:07:03 -0400

    outra nova correção do settings.py para acertara configuração do heroku

--- creatusFilms/settings.py
+++ creatusFilms/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'creatusFilms.urls'
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = 'staticfiles/'
 
 STATIC_URL = '/static/'
 

83484f0285a19e44139fbfe4c2a6a2b52f5f7cc7
Romulo Sousa <carmosousa20@outlook.com>
2022-09-06 23:49:16 -0400

    Corrigindo importações

--- Films/views.py
+++ Films/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Films
+
 # Create your views here.
 
-def PaginaInicial(request):
-    lista_filmes = Films.objects.All()
 
-    return render(request, 'index.html',{'listaFilms':lista_filmes})
+def PaginaInicial(request):
+    lista_filmes = Films.objects.all()
+
+    return render(request, "index.html", {"listaFilms": lista_filmes})
